6c3ae89c2dff0d0bc983c033572965e456d91a0f
Clinton Judy <clinton@westarete.com>
2011-04-20 14:12:07 -0400

    Modified theme to use only 16 colors, so we can used Solarized's theme. Or any other theme of your choosing, really.

--- themes/judy_256.zsh
+++ themes/judy_256.zsh
@@ -1,17 +1,14 @@
-# zsh theme requires 256 color enabled terminal
-# i.e TERM=xterm-256color
+PROMPT='$(git_prompt_info)$(pair_prompt_info)$(rvm_prompt_info) %F{yellow}%c%f
+%B%F{7}〓%f%b %{$reset_color%}'
+RPROMPT='%F{3}%n%f%{$reset_color%}@%F{4}%m%f%{$reset_color%}'
 
-PROMPT='$(git_prompt_info)$(pair_prompt_info)$(rvm_prompt_info) %F{208}%c%f
-%B%F{154}〓%f%b %{$reset_color%}'
-RPROMPT='%F{208}%n%f%{$reset_color%}@%F{039}%m%f%{$reset_color%}'
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%F{236}±❮%F{244}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%F{236}❯%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%F{10}±❮%F{8}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
 
-ZSH_THEME_PAIR_PROMPT_PREFIX="%F{236}❮%f%F{067}"
-ZSH_THEME_PAIR_PROMPT_SUFFIX="%F{236}❯%{$reset_color%}"
+ZSH_THEME_PAIR_PROMPT_PREFIX="%F{10}❮%f%F{13}"
+ZSH_THEME_PAIR_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
 
-ZSH_THEME_RVM_PROMPT_PREFIX="%F{236}❮%f%F{089}"
-ZSH_THEME_RVM_PROMPT_SUFFIX="%F{236}❯%{$reset_color%}"
+ZSH_THEME_RVM_PROMPT_PREFIX="%F{10}❮%f%F{6}"
+ZSH_THEME_RVM_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
