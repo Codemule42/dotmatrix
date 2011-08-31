14ec733f1651caa3de913f66c6d107c02b69aa81
Clinton Judy <clinton@westarete.com>
2011-08-31 10:39:07 -0400

    Change theme for 'tomorrow night' color scheme.

--- themes/judy_256.zsh
+++ themes/judy_256.zsh
@@ -1,14 +1,14 @@
-PROMPT='$(git_prompt_info)$(pair_prompt_info)$(rvm_prompt_info) %F{yellow}%c%f
+PROMPT='$(git_prompt_info)$(pair_prompt_info)$(rvm_prompt_info) %F{11}%c%f
 %B%F{7}〓%f%b %{$reset_color%}'
-RPROMPT='%F{3}%n%f%{$reset_color%}@%F{4}%m%f%{$reset_color%}'
+RPROMPT='%F{12}%n%F{8}@%F{13}%m%f%{$reset_color%}'
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%F{10}±❮%F{8}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%F{8}±❮%F{8}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%F{8}❯%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%F{1}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%F{10}"
 
-ZSH_THEME_PAIR_PROMPT_PREFIX="%F{10}❮%f%F{13}"
-ZSH_THEME_PAIR_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
+ZSH_THEME_PAIR_PROMPT_PREFIX="%F{8}❮%f%F{14}"
+ZSH_THEME_PAIR_PROMPT_SUFFIX="%F{8}❯%{$reset_color%}"
 
-ZSH_THEME_RVM_PROMPT_PREFIX="%F{10}❮%f%F{6}"
-ZSH_THEME_RVM_PROMPT_SUFFIX="%F{10}❯%{$reset_color%}"
+ZSH_THEME_RVM_PROMPT_PREFIX="%F{8}❮%f%F{9}"
+ZSH_THEME_RVM_PROMPT_SUFFIX="%F{8}❯%{$reset_color%}"
